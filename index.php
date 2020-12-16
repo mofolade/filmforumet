34f28e897b64eb599397024b157b2b7ff6937538
Aniko Magyar <mofolade@gmail.com>
2020-12-16 14:21:58 +0100

    Index page.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>      
+        <div class="App" id="app">
+            <div class="d-flex flex-grow-1 h-100">
+            </div>
+        </div>
+    </body>
+</html>
