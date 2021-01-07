d0ddb42e525f1873ff5c6b1f317609877756f05e
Aniko Magyar <mofolade@gmail.com>
2021-01-07 23:18:01 +0100

    fixed profile box

--- mypage.php
+++ mypage.php
@@ -8,7 +8,14 @@
         include_once 'views/head.php'; 
 
         include_once 'src/ACLSettingsClass.php';
-        $ACLSettings = new ACLSettingsClass();    
+        $ACLSettings = new ACLSettingsClass();
+
+        if (!empty($_SESSION["user_id"])) {
+            include_once './src/DB/UserClass.php';
+            $user = new UserClass();
+            $currentUser = $user->getUser($_SESSION["user_id"]);
+            $currentUser = json_decode($currentUser, true);
+        }
 
 
     ?>
@@ -17,7 +24,16 @@
             <main>
                 <?php include 'views/header.php';?>
                 <div class="wrapper">
-                    <div id="topics-cover">
+                    <div id="profile-cover">
+                        <div class="profile">
+                            <?php
+                            if(!empty($_SESSION["user_id"])){
+                                echo '<img src=".'.$currentUser['picture_url'].'" alt="" class="profile_image">';
+                                echo '<div class="profile_name">'.$currentUser['name'].'</div>';
+                                echo '<div class="profile_email">'.$currentUser['email'].'</div>';
+                            }
+                            ?>
+                        </div>
                     </div>
                 </div>
             </main>
